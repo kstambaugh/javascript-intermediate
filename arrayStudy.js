c6bc0ffbab16d09f670b4c3ed0fb9030171a4b7b
kstambaugh <refinancethekraken@gmail.com>
2022-03-31 19:36:33 -0400

    added for loop to array

---
+++ arrayStudy.js
@@ -0,0 +1,13 @@
+const array = ["kyle","caleb","crystal","devon"]
+const food = "hotdog"
+const foods = ["pizza", "pasta", "hotdog", "hamburger"]
+
+
+
+for(let i =0;i<array.length;i++ ){
+    console.log(array[i],"likes",foods[i]+".")
+}
+
+// for(let i = array.length-1;i>0; i--){
+//     console.log(array[i])
+// }
