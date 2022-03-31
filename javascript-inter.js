4cd2b5ed5e942f903d94a14542169b2504303a92
kstambaugh <refinancethekraken@gmail.com>
2022-03-31 19:21:47 -0400

    making nested arrays

--- javascript-inter.js
+++ javascript-inter.js
@@ -3,20 +3,59 @@
 //console.log(awesomeClass[4])
 
 
-const breakfast = "eggs"
-const lunch = "pizza"
-const dinner = "sushi"
+// const breakfast = "eggs"
+// const lunch = "pizza"
+// const dinner = "sushi"
 
-const myFood = [breakfast, lunch, dinner]
-console.log(myFood)
+// const myFood = [breakfast, lunch, dinner]
+// console.log(myFood)
 
-const classFoodArray = ["pizza", "pasta", "hotdog", "hamburger"]
-const classNames = ["kyle","mary","david","tim"]
-const classFoodsFavs = {kyle:"pizza", mary:"pasta", david: "hotdog", tim:"hamburger"}
+// const classFoodArray = ["pizza", "pasta", "hotdog", "hamburger"]
+// const classNames = ["kyle","mary","david","tim"]
+// const classFoodsFavs = {kyle:"pizza", mary:"pasta", david: "hotdog", tim:"hamburger"}
 
-console.log(classFoodsFavs)
-console.log(classNames);
-console.log(classFoodArray)
+// console.log(classFoodsFavs)
+// console.log(classNames);
+// console.log(classFoodArray)
 
-console.log(classFoodsFavs["kyle"])
-console.log(classFoodsFavs.kyle)
+// console.log(classFoodsFavs["kyle"])
+// console.log(classFoodsFavs.kyle)
+
+// const dndClasses = ["artificer","barbarian","bard","cleric","druid","fighter","monk","paladin","ranger","rogue","sorcerer","warlock","wizard"]
+// const playerName = ["cameron","kyle","jae","erick","kashmir","mae","anthony","brandom","amanda","jen","larry"]
+// const daysOfWeek = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+
+const kyleProfile = {
+    profileID:["123456789"],
+    address:{
+        state:"VA",
+        city:"Virginia Beach",
+        street:"Jove Dr.",
+        zip: "23462"
+    },
+    name:{
+        first:"Kyle",
+        last:"Stambaugh"
+    },
+    phone:{
+        primary:"757-867-5309",
+        secondary:"999-999-9999"
+    },
+    music:["country", "rock","alterative"],
+    games:["skyrim","dnd","chess","catan","minecraft"],
+    emergencyContacts:{
+        primaryContact:{
+            firstName:"Kashmir",
+            lastName:"Stambaugh",
+            relation:"spouse",
+            phone:"987-654-3210"
+        },
+        secondaryContact:{
+            firstName:"Gloria",
+            lastName:"Skorija",
+            relation:"parent",
+            phone:"987-654-3210"
+        }
+
+    }
+}
