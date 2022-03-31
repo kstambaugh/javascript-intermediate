ae64f6a7a7b44828e6f505d96cbefa0fa8186c04
kstambaugh <refinancethekraken@gmail.com>
2022-03-31 18:50:50 -0400

    updated objects and arrays

--- javascript-inter.js
+++ javascript-inter.js
@@ -1,3 +1,22 @@
 // array and object- an array is a type of object. 
-const awesomeClass = ["student1", "student2", "student3","teacher"]
-console.log(awesomeClass[4])
+//const awesomeClass = ["student1", "student2", "student3","teacher"]
+//console.log(awesomeClass[4])
+
+
+const breakfast = "eggs"
+const lunch = "pizza"
+const dinner = "sushi"
+
+const myFood = [breakfast, lunch, dinner]
+console.log(myFood)
+
+const classFoodArray = ["pizza", "pasta", "hotdog", "hamburger"]
+const classNames = ["kyle","mary","david","tim"]
+const classFoodsFavs = {kyle:"pizza", mary:"pasta", david: "hotdog", tim:"hamburger"}
+
+console.log(classFoodsFavs)
+console.log(classNames);
+console.log(classFoodArray)
+
+console.log(classFoodsFavs["kyle"])
+console.log(classFoodsFavs.kyle)
